41167d2e057027fda3f1219841ae0e94c72c7af9
nguyenduc <1409.nguyenduc@gmail.com>
2022-12-05 21:08:23 +0700

    [29/11] Create Customer entity and connect to postgres in docker.

--- customer/src/main/java/com/nguyenduc/customer/CustomerService.java
+++ customer/src/main/java/com/nguyenduc/customer/CustomerService.java
@@ -1,6 +1,9 @@
 package com.nguyenduc.customer;
 
-public record CustomerService() {
+import org.springframework.stereotype.Service;
+
+@Service
+public record CustomerService(CustomerRepository customerRepository) {
     public void registerCustomer(CustomerRegistrationRequest request) {
         Customer customer = Customer.builder()
                 .firstName(request.firstName())
@@ -10,6 +13,6 @@ public record CustomerService() {
 
         // TODO: check if email valid
         // TODO: check if email not taken
-        // TODO: store customer in db
+        customerRepository.save(customer);
     }
 }
