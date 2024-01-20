03bd1228fa142693b7405a554518c2957907c480
nguyenduc <1409.ducnguyen@gmail.com>
2024-01-20 14:18:11 +0700

    15/01 Create Eureka server, config Eureka client for Customer and Fraud.

--- fraud/src/main/java/com/nguyenduc/fraud/FraudController.java
+++ fraud/src/main/java/com/nguyenduc/fraud/FraudController.java
@@ -1,6 +1,7 @@
 package com.nguyenduc.fraud;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -9,6 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("api/v1/fraud-check")
 @RequiredArgsConstructor
+@Slf4j
 public class FraudController {
 
     private final FraudCheckHistoryService fraudCheckHistoryService;
@@ -16,7 +18,7 @@ public class FraudController {
     @GetMapping("{customerId}")
     public FraudCheckResponse isFraudster(@PathVariable("customerId") Integer customerId) {
         boolean isFraudulentCustomer = fraudCheckHistoryService.isFraudulentCustomer(customerId);
-
+        log.info("[FraudController] isFraudster: {}", customerId);
         return new FraudCheckResponse(isFraudulentCustomer);
     }
 }
